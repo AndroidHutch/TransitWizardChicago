a58bbb1101787878d2b8c3e9bd20e8db03b65792
hutchTheGuy <mikehutch06@gmail.com>
2011-03-11 06:27:32 +0000

    adding files

---
+++ src/com/hutchdesign/transitgenie/TransitGenieMain.java
@@ -0,0 +1,13 @@
+package com.hutchdesign.transitgenie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TransitGenieMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
